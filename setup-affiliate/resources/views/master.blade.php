1d8d726f81635f91ca566ec40b97f821ad2994b0
prachi8561-eng <prachi8561@gmail.com>
2025-11-13 10:03:11 +0530

    changes

--- setup-affiliate/resources/views/master.blade.php
+++ setup-affiliate/resources/views/master.blade.php
@@ -7,7 +7,8 @@
         <!-- Add your CSS links here -->
       
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     </head>
     <style>
         .bg-dark.text-white.py-4.position-relative{
