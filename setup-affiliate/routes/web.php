66622034fdd6c4b2ff1b552788355bfbd53452cf
prachi8561-eng <prachi8561@gmail.com>
2025-11-13 14:41:24 +0530

    changes

--- setup-affiliate/routes/web.php
+++ setup-affiliate/routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->name('home');
+})->name('dashboard');
+
 Route::get('/user', function () {
     return view('user_dashboard');
 })->name('user_dash');
