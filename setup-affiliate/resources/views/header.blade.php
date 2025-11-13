1a33c0f37712f0735b6768a09954b58a6a556083
prachi8561-eng <prachi8561@gmail.com>
2025-11-13 13:13:32 +0530

    changes

--- setup-affiliate/resources/views/header.blade.php
+++ setup-affiliate/resources/views/header.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">
+    <img src="{{ asset('upload/logo.png') }}" alt="Logo 6" class="logo-img">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
