c82fb084e696d3e81977e57fb9f062bfcd69547c
prachi8561-eng <prachi8561@gmail.com>
2025-11-14 15:50:53 +0530

    Update user_header.blade.php

--- setup-affiliate/resources/views/user_header.blade.php
+++ setup-affiliate/resources/views/user_header.blade.php
@@ -1,60 +1,68 @@
-<nav class="navbar navbar-expand-lg bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
+
     <a class="navbar-brand" href="#">
-    <img src="{{ asset('upload/logo.png') }}" alt="Logo 6" class="logo-img">
+      <img src="{{ asset('upload/logo.png') }}" class="logo-img" alt="Logo">
     </a>
-    <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon text-light"></span>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <!-- Left Menu -->
       <ul class="navbar-nav m-auto mb-2 mb-lg-0">
         <li class="nav-item">
-        <a class="nav-link active text-light" aria-current="page" href="{{route('user_dash')}}">Home</a>
+          <a class="nav-link {{ request()->routeIs('user_dash') ? 'active' : '' }}" href="{{ route('user_dash') }}">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" href="{{route('user_referrals')}}">Referrals</a>
+          <a class="nav-link {{ request()->routeIs('user_referrals') ? 'active' : '' }}" href="{{ route('user_referrals') }}">Referrals</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" href="{{route('user_comissions')}}">Commisions</a>
+          <a class="nav-link {{ request()->routeIs('user_comissions') ? 'active' : '' }}" href="{{ route('user_comissions') }}">Commissions</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" href="{{route('user_payouts')}}">Payouts</a>
+          <a class="nav-link {{ request()->routeIs('user_payouts') ? 'active' : '' }}" href="{{ route('user_payouts') }}">Payouts</a>
         </li>
       </ul>
+
+      <!-- Right Auth Section -->
       <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+        @guest
+            <li class="nav-item">
+                <a class="nav-link text-light" href="{{ route('login') }}">Login</a>
+            </li>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+            <li class="nav-item">
+                <a class="nav-link text-light" href="{{ route('register') }}">Register</a>
+            </li>
+        @else
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown"
+                   role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                   {{ Auth::user()->name }}
+                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a class="dropdown-item" href="#"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                       Logout
+                    </a>
+                  </li>
+                </ul>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
+        @endguest
+
+      </ul>
     </div>
   </div>
 </nav>
