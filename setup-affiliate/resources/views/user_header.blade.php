9cf95cf48c5fec9a6fca731b08bf2c75c1c8cec3
prachi8561-eng <prachi8561@gmail.com>
2025-11-17 08:16:02 +0530

    Update user_header.blade.php

--- setup-affiliate/resources/views/user_header.blade.php
+++ setup-affiliate/resources/views/user_header.blade.php
@@ -28,41 +28,6 @@
           <a class="nav-link {{ request()->routeIs('user_payouts') ? 'active' : '' }}" href="{{ route('user_payouts') }}">Payouts</a>
         </li>
       </ul>
-
-      <!-- Right Auth Section -->
-      <ul class="navbar-nav ms-auto">
-
-        @guest
-            <li class="nav-item">
-                <a class="nav-link text-light" href="{{ route('login') }}">Login</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link text-light" href="{{ route('register') }}">Register</a>
-            </li>
-        @else
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown"
-                   role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                   {{ Auth::user()->name }}
-                </a>
-
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a class="dropdown-item" href="#"
-                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                       Logout
-                    </a>
-                  </li>
-                </ul>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </li>
-        @endguest
-
-      </ul>
     </div>
   </div>
 </nav>
